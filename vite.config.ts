18f85b6c5ed3db6737e10d212b1fcd286715bb1a
ice <ice_cola_yyds@163.com>
2022-09-07 22:58:53 +0800

    配置别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path' // 备注1：在 ts 模块中加载 node 核心模块需要安装 node 的类型补充模块：pnpm add -D @types/node
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  base: '/a/b/',
+  resolve: {
+    alias: { 
+      '@': resolve(__dirname, 'src')
+    }
+  }
 })
